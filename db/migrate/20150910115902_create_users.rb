4f617be7ec48aa66f143a0074f7d26a3034f23ac
mizunami <takaya1258@gmail.com>
2015-09-15 13:24:05 +0000

    user-profile

--- db/migrate/20150910115902_create_users.rb
+++ db/migrate/20150910115902_create_users.rb
@@ -4,8 +4,6 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :email
       t.string :password_digest
-      t.string :location
-      t.string :description
 
       t.timestamps null: false
 
